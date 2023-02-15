8066ca2c9d1a15be02a7879f20133162004fa0b2
chinaTenCode <hack0b@163.com>
2023-02-15 18:15:44 +0800

    create server;

---
+++ un-server/src/test/java/com/qiaweidata/undercurrent/UndercurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaweidata.undercurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UndercurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
