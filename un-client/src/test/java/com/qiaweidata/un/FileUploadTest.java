4412c0f320b20cbe43d371ca44d615967cb12acd
chinaTenCode <hack0b@163.com>
2023-03-13 18:17:32 +0800

    upload file test;

---
+++ un-client/src/test/java/com/qiaweidata/un/FileUploadTest.java
@@ -0,0 +1,85 @@
+package com.qiaweidata.un;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @Title: FileUploadTest
+ * @Description: FileUploadTest
+ * @Company: www.qiaweidata.com
+ * @author: shenshilong
+ * @date: 2023-03-13
+ * @version: V1.0
+ */
+public class FileUploadTest {
+
+    public static void main(String[] args) {
+
+        try (FileInputStream input = new FileInputStream("E:\\git\\qiwen-file-master\\README.md");) {
+            doPost(input.readAllBytes(), "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String doPost( byte[] fileBytes, String fileName) {
+
+        //拿到fileName拼接URL
+        StringBuffer sb=new StringBuffer();
+        final String url = sb.append("http://localhost:8081").append(fileName).toString();
+        //创建HttpClient实例
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //创建post方法连接实例，在post方法中传入待连接地址
+        HttpPost httpPost = new HttpPost(url);
+        CloseableHttpResponse response = null;
+
+        try {
+            //设置请求参数（类似html页面中name属性）
+            MultipartEntityBuilder entity = MultipartEntityBuilder.create();
+            entity.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            entity.setCharset(Charset.forName("UTF-8"));
+            if(fileBytes!=null) {
+                //内容类型，用于定义网络文件的类型和网页的编码，决定文件接收方将以什么形式、什么编码读取这个文件
+                ContentType OCTEC_STREAM = ContentType.create("application/octet-stream", Charset.forName("UTF-8"));
+                //添加文件
+                entity.addBinaryBody("importSource", fileBytes, OCTEC_STREAM, fileName);
+            }
+            entity.addTextBody("sourceId", "4028818982e871ab0182e8f53fac0099");
+
+            httpPost.setEntity(entity.build());
+            httpPost.setHeader("Cookie","JSESSIONID=B7C9690454870704B431C735B46D5230");
+
+
+
+            //发起请求，并返回请求响应
+            response = httpClient.execute(httpPost);
+            String uploadResult = EntityUtils.toString(response.getEntity(), "utf-8");
+
+
+            System.out.println(uploadResult);
+            return uploadResult;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "文件上传错误";
+    }
+
+}
--- un-client/src/test/java/com/qiaweidata/un/h2/StoreSystemFile.java
+++ un-client/src/test/java/com/qiaweidata/un/h2/StoreSystemFile.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * @Title: StoreSystemFile
  * @Description: StoreSystemFile
- * @Company: www.wrenchdata.com
+ * @Company: www.qiaweidata.com
  * @author: shenshilong
  * @date: 2023-03-11
  * @version: V1.0
