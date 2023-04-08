39671487964ba7db8dfb1e03e1f53b274342d84d
chinaTenCode <hack0b@163.com>
2023-04-08 13:52:51 +0800

    main method;

--- un-client/src/test/java/com/qiaweidata/un/DingUploadTest.java
+++ un-client/src/test/java/com/qiaweidata/un/DingUploadTest.java
@@ -15,7 +15,6 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-import org.junit.Test;
 
 /**
  * @Title: DingUploadTest
@@ -40,12 +39,10 @@ public class DingUploadTest {
         }
     }
 
-    @Test
     public void getApiInfo() {
 
     }
 
-    @Test
     public void singleFileUpload() throws IOException {
 
         // 从接口返回信息中拿到resourceUrls
@@ -83,7 +80,6 @@ public class DingUploadTest {
         }
     }
 
-    @Test
     public void corpId() {
 
         HashMap<String, Object> paramMap = new HashMap<>();
@@ -101,5 +97,21 @@ public class DingUploadTest {
         System.out.println(result2);
     }
 
+    public static void main(String[] args) {
+
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("corpid", properties.get("corpid"));
+        paramMap.put("ssoSecret", properties.get("ssosecret"));
+
+        //String result= HttpUtil..post("https://api.dingtalk.com/v1.0/oauth2/ssoAccessToken", paramMap);
+        //链式构建请求
+        String result2 = HttpRequest.post("https://api.dingtalk.com/v1.0/oauth2/ssoAccessToken")
+            .header(Header.CONTENT_TYPE, "application/json")//头信息，多个头信息多次调用此方法即可
+            .form(paramMap)//表单内容
+            .timeout(20000)//超时，毫秒
+            .execute().body();
+        Console.log(result2);
+        System.out.println(result2);
+    }
 
 }
