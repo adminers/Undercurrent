54b2f9e48e470fa9d21295c1a62adae00152be67
adminers <1019358770@qq.com>
2023-04-14 11:46:24 +0800

    jsp util;

---
+++ un-client/src/test/java/com/qiaweidata/un/JspUtil.java
@@ -0,0 +1,30 @@
+package com.qiaweidata.un;
+
+/**
+ * @Title: JspUtil
+ * @Description: JspUtil
+ * @Company: www.wrenchdata.com
+ * @author: shenshilong
+ * @date: 2023-04-14
+ * @version: V1.0
+ */
+public class JspUtil {
+
+    /**
+     * 批量替换 JSP 页面的 java 代码
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // 读取文件夹下的所有 jsp 文件
+
+        // 读取到特殊标记,替换/追加代码
+
+        // 识别 css,js 等 html 元素的标记,插入版本号
+
+        // 把修改后的 jsp 和修改前的 jsp,分别记录到一个文件中,方便用 vscode 做对比
+
+
+    }
+}
