3a3d914708b8e61f1a8b8e5222001fc9007be8a8
adminers <1019358770@qq.com>
2023-04-20 14:05:33 +0800

    RestfulFastRequest;

---
+++ un-client/src/test/java/com/qiaweidata/un/plugin/RestfulFastRequestTest.java
@@ -0,0 +1,12 @@
+package com.qiaweidata.un.plugin;
+
+/**
+ * @Title: RestfulFastRequestTest
+ * @Description: RestfulFastRequestTest
+ * @Company: www.wrenchdata.com
+ * @author: shenshilong
+ * @date: 2023-04-20
+ * @version: V1.0
+ */
+public class RestfulFastRequestTest {
+}
