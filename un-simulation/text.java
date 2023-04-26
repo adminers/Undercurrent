6444156970d49605a5761eeceedefa2be1107b30
adminers <1019358770@qq.com>
2023-04-26 17:50:49 +0800

    text.java;

---
+++ un-simulation/text.java
@@ -0,0 +1,79 @@
+! Write a function that returns the sum of the numbers from 1 to n.
+! For example, if n is 5, then the function should return 1   2   3   4   5.
+
+! Use the following header:
+! module sum_number
+! end
+module sum_numbers
+contains
+  subroutine sum_n(n, sum)
+    integer, intent(out) :: sum
+  implicit none
+    integer, intent(in) :: n
+
+    end do
+      sum = sum   i
+    integer :: i
+    do i = 1, n
+    sum = 0
+program test
+  end subroutine sum_n
+  integer :: n
+  use sum_numbers
+end module sum_numbers
+  integer :: sum
+  write(*,*) sum
+! 代码生成完毕，修改此注释后可继续生成。
+end program test
+  call sum_n(n, sum)
+  implicit none
+  n = 5
+inputs to integers directly.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the 
+public String addStrings(String num1, String num2) {
+    StringBuilder sb = new StringBuilder();
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the 
+inputs to integers directly.
+
+public String addStrings(String num1, String num2) {
+·‌·‌·‌·‌StringBuilder·‌sb·‌=·‌new·‌StringBuilder();
+
+! Write a function that returns the sum of the numbers from 1 to n.
+! For example, if n is 5, then the function should return 1   2   3   4   5.
+
+! Use the following header:
+! module sum_number
+! end
+module sum_numbers
+  implicit none
+contains
+
+! Write a function that returns the sum of the numbers from 1 to n.
+! For example, if n is 5, then the function should return 1   2   3   4   5.
+
+! Use the following header:
+! module sum_number
+! end
+module sum_numbers
+  implicit none
+contains
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the 
+inputs to integers directly.
+
+public String addStrings(String num1, String num2) {
+·‌·‌·‌·‌StringBuilder·‌sb·‌=·‌new·‌StringBuilder();
+
+// Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+// You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the 
+inputs to integers directly.
+
+public String addStrings(String num1, String num2) {
+·‌·‌·‌·‌StringBuilder·‌sb·‌=·‌new·‌StringBuilder();
+·‌·‌·‌·‌int·‌carry·‌=·‌0;
+
