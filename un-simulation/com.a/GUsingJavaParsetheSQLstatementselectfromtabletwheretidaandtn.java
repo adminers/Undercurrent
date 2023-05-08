9ade7d4584ca3b0085766b9f04159ebe9019d57c
adminers <1019358770@qq.com>
2023-05-08 14:00:43 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUsingJavaParsetheSQLstatementselectfromtabletwheretidaandtn.java
+++ un-simulation/com.a/GUsingJavaParsetheSQLstatementselectfromtabletwheretidaandtn.java
@@ -1,9 +1,9 @@
 // G Using Java8, Parse the SQL statement 'select * from table t where t. id=${a} and t. name=${b} and t. name=${c}' using regular parsing, replacing all ${a} ${b} ${c} with '?' and outputting it to the console
-. If there are no parameters, output'select * from table'. If there are parameters, output'select * from table where t. id=? and t. name=? and t. name=?
+//. If there are no parameters, output'select * from table'. If there are parameters, output'select * from table where t. id=? and t. name=? and t. name=?
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Test {
+public class GUsingJavaParsetheSQLstatementselectfromtabletwheretidaandtn {
     public static void main(String[] args) {
         String sql = "select * from table where id =? and name =? and name =?";
         Pattern pattern = Pattern.compile("\\${(.*?)}");
