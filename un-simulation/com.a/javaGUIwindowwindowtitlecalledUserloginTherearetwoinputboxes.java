35ae18ddfdb88a8870d2f23e540be747d8a3d70f
adminers <1019358770@qq.com>
2023-05-15 18:05:27 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/javaGUIwindowwindowtitlecalledUserloginTherearetwoinputboxes.java
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // java8, GUI window, window title called: User login. There are two input boxes called username and password. There are two buttons, one called submit and one called cancel
+===......
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class Test extends JFrame {
+ syntax.H..Open. syntaxUsually,. =.=,#H=..H.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.H.Open. syntax. syntax. syntax/. syntaxDs syntax syntaxDs syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax =
+\Usually\. syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz C header names syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
