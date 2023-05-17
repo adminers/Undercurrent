35c104c79ca301eb5c9c734adccecbef65c5c198
adminers <1019358770@qq.com>
2023-05-17 18:00:01 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/JavaObtainsthenumberofnicsinthelocalsystemAndoutputtothecons.java
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // Java Obtains the number of nics in the local system. And output to the console.
+ - - [ - syntax==.. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. ] - -
+. [ syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz C header names syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz C header names syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
