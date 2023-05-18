78c0c948b77961c3b58a0b99dd147afba39113b5
adminers <1019358770@qq.com>
2023-05-18 18:12:31 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/Createanemptyprojectfortheideaplugin.java
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // Create an empty project for the idea plug-in
+.................. [ - syntax. syntax. syntax. syntax. syntax. syntax. ]
+import java.io.IOException;
+=:. syntaxUsually =. [ - syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax. ]
+..= syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax. [ - syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz C header names syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax   
+  </code></pre>
+
+</body>
+</html>
