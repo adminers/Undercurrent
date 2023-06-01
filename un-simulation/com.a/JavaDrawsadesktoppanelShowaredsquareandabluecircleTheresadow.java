00346e6c958ae1b76516a55ada6999b49fc23703
Administrator <1019358770@qq.com>
2023-06-01 17:36:14 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/JavaDrawsadesktoppanelShowaredsquareandabluecircleTheresadow.java
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // Java Draws a desktop panel. Show a red square and a blue circle. There's a download button. You can export graphics into pictures;
+ png, jpg, eps, pdf, svg, and bitmap. Support for multiple monitors and windows.
+import java.awt.*;
+import java.awt.event.*;
+H,.XY acurlspecial plusH,..LabelU ac,H,H fieldH fieldHurl field span field, field field,,,
+{
+  public static void main(String[] a) { new acurlspecial(); }
+  public acurlspecial() {
+    super("acurlspecial",new Dimension(640,640));
+    setBackground(Color.white);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+::,:H,.,.,.H.,:LabelLabelU:.,label span,H field,,H field,,H field,,Out,,Out,,Out span,,Out span,,H,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,Out span,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,H field,,
+<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
