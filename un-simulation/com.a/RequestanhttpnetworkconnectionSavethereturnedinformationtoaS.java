3c68699581e016431005163ca8ff40ce64495e1f
Administrator <1019358770@qq.com>
2023-06-09 14:00:02 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/RequestanhttpnetworkconnectionSavethereturnedinformationtoaS.java
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // Request an http network connection. Save the returned information to a String. Output to the console.\n
+" +
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HttpRequest {
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://codegeex.cn");
+        URLConnection conn = url.openConnection();
+        conn.connect();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = in.readLine())!= null)
+            System.out.println(line);
+        in.close();
+    }
+}   
+  </code></pre>
+
+</body>
+</html>
