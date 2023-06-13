12bfa0a9a9e4f3c5c7fc6ae351a32ce1a2ebc515
Administrator <1019358770@qq.com>
2023-06-13 10:00:01 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/javapagingListcodeOptimizeforsimplicityn.java
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // java paging List code. Optimize for simplicity.\n
+Usually syntaxUsually,H.Usually. {H {H syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax[ - syntax]}}\n\n(Note: this is not a task for pager or syntax highlighting, just a quick reference.)"
+public class Pager {
+. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax=" syntax syntax. syntax. syntax. syntaxDomain syntax. syntax. syntax. syntax. syntax. syntax. syntax [ syntax syntax [ syntax syntaxHexportUsually syntax syntaxHEUsually. syntax syntaxH syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. ]]";
+.................................................................
+ C.Usually. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax [ syntax [ syntax syntaxH syntaxH syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. ]]
+ D.Usually. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax [ syntax [ syntax syntaxH syntaxH syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz Class syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxnameName<==<========== syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
