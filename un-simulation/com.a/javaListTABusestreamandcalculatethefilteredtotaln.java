633223fa09b4eaa553887908f02ec57664d5c477
Administrator <1019358770@qq.com>
2023-06-14 16:33:48 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/javaListTABusestreamandcalculatethefilteredtotaln.java
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // java8,List TAB, use stream() and calculate the filtered total.\n
+" +
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Example {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+/ syntax. syntaxUsually or. syntax. syntax. syntaxUsuallyH. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax.
+        List<Integer> filtered = numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .mapToInt(x -> x * x)
+ {. syntax. syntaxXXX syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax. syntax syntax. syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntaxz C header \ {     描述 ======<=< Index
+Summary the syntax in use? How about the <, >, and | characters?
+Summary the syntax in use? How about the syntax in use?
+<div>
+<div></div>
+ syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax </div>
+ -. syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax syntax<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
