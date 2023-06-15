0619f913ad3fd83f292a5a09cf21770f3629ef8b
Administrator <1019358770@qq.com>
2023-06-15 17:00:02 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/SwingGUIdrawawindowThewindowhasinputfieldsandbuttonsn.java
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // Swing GUI, draw a window. The window has 100 input fields and 100 buttons.\n
+" +
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Test extends JFrame {
+    private JLabel label;
+    private JPanel buttonPanel;
+    private JTextField textField;
+    private JButton button;
+    private int count = 0;
+    private int max = 100;
+    private int min = 0;
+    private int value;
+    private int step = 1;
+    private int delay = 100;
+    private boolean isRunning = false;
+    private int id;
+    private int maxId;
+    private int minId;
+    private int valueId;
+    private int stepId;
+    private int delayId;
+    private int countId;
+    private int maxId;
+    private int minId;
+    private int valueId;
+    private int stepId;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delay;
+    private int id;
+    private int max;
+    private int min;
+    private int value;
+    private int step;
+    private int delay;
+    private int count;
+    private int maxId;
+    private int minId;
+    private int valueId;
+    private int stepId;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delay;
+    private int countId;
+    private int maxId;
+    private int minId;
+    private int valueId;
+    private int stepId;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+    private int delayId;
+    private int value;
+    private int step;
+   
+  </code></pre>
+
+</body>
+</html>
