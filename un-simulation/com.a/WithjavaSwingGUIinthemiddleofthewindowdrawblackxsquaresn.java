24e10878b03c78082ad9e1728e1ffd315e82bdc1
Administrator <1019358770@qq.com>
2023-06-16 16:45:04 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/WithjavaSwingGUIinthemiddleofthewindowdrawblackxsquaresn.java
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n
+" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+//          "With java8,Swing GUI, in the middle of the window, draw 30 black 10x10 squares;\n" +
+<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
