27091d9736b557bf916ff882b792a6bef04dc030
Administrator <1019358770@qq.com>
2023-06-27 09:03:19 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -20,15 +20,37 @@
 
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
-不要使用等离子效果, 否则会引起严重不满。
-import java.util.Random;
+" +
+"创造一个集合A={1,2,3,4,5}，并且创建一个集合B={¬,¬,1,2,¬,4,¬,5,¬}，每个¬符号对应于集合A中不存在的元素。\n" +
+"B不会生成任何内容，只是为了注明，方法还应该给出 A ∪ B 的示例。 （总之，A ∪ B 就是两个集合的并集）。另请参见：环境中的集合的创建。";
 
-public class RandomExample {
+public class SetOperations {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int a = rand.nextInt();
+        Set<String> setA = new HashSet<>();
+        setA.add("1");
+        setA.add("2");
+        setA.add("3");
+        setA.add("4");
+        setA.add("5");
+        Set<String> setB = new HashSet<>();
+        setB.add("¬");
+        setB.add("¬");
+        setB.add("1");
+        setB.add("2");
+        setB.add("4");
+        setB.add("¬");
+        setB.add("5");
+        setB.add("¬");
+        System.out.println("A ∪ B = " + setA.union(setB));
+        System.out.println("A ∩ B = " + setA.intersect(setB));
+        System.out.println("A - B = " + setA.subset(setB));
+        System.out.println("B - A = " + setB.subset(setA));
+        System.out.println("A ⊆ B = " + setA.subset(setB));
+        System.out.println("B ⊆ A = " + setB.subset(setA));
+        System.out.println("A ≠ B = " + setA.equals(setB));
     }
-}   
+}
+   
   </code></pre>
 
 </body>
