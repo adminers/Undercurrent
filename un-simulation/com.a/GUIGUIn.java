7d90df4dbbb08f437d93ea56596e01f4dbe2db7d
Administrator <1019358770@qq.com>
2023-07-03 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,24 +21,35 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 " +
+        "创造一个窗口并在其中绘制一个黑色正方形。点击按钮即可绘制正方形。";
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 
-public class WindowBuilderExample extends JFrame {
-    public WindowBuilderExample() {
-        super("WindowBuilder Example");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().add(new JButton("Push Me!"), BorderLayout.CENTER);
-        pack();
-        setVisible(true);
-    }
+public class SimpleWindow {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new WindowBuilderExample());
+        JFrame frame = new JFrame("Simple Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        JButton button = new JButton("Draw a rectangle");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Drawing a rectangle");
+            }
+        });
+        frame.add(button, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
-   
+
+class ActionListener implements java.awt.event.ActionListener {
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Drawing a circle");
+    }
+}   
   </code></pre>
 
 </body>
