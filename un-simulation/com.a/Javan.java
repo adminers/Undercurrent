b870f1001f1ac188130c815cf9aaa69d017c64c2
Administrator <1019358770@qq.com>
2023-07-09 09:10:47 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,55 +21,70 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 给你一个整数数组 nums ，请你在该数组中找出由三个数组成的最大乘积，并输出这个乘积。\n
-// 示例 1：\n
-// 输入：nums = [1,2,3]\n
-// 示例 2：\n
-// 输入：nums = [1,2,3,4]\n
-// 示例 3：\n
-// 输入：nums = [-1,-2,-3]\n
-// 示例 4：\n
-// 输入：nums = [-1]\n
-// 输出：-1\n
-// 解释：数组中没有元素。\n
-// 示例 5：\n
-// 输入：nums = [0]\n
-// 输出：0\n
-// 示例 6：\n
-// 输入：nums = [-1,-2,-3,0]\n
-// 输出：0\n
-// 示例 7：\n
-// 输入：nums = [-1,-2,-3,0,1]\n
-// 输出：1\n
-// 示例 8：\n
-// = [-1,-2,-3,0,1,2,3]\n
-// 输出：6\n
-/// 提示：\n
-// 3 <= nums.length <= 10^4\n
-// -1000 <= nums[i] <= 1000\n
-// -10^4 <= sum(nums[i]) <= 10^4\n
-
-
-package com.vinci.code.leetcode.editor.cn;
-
-public class MaximumProductOfThreeNumbers {
-    public static void main(String[] args) {
-         Solution solution = new MaximumProductOfThreeNumbers().new Solution();
-    }
-    
-
-class Solution {
-    public int maximumProduct(int[] nums) {
-        if (nums.length < 3) {
-            return 0;
-        }
-        Arrays.sort(nums);
-        int len = nums.length;
-        return Math.max(nums[0] * nums[1] * nums[len - 1], nums[len - 3] * nums[len - 2] * nums[len - 1]);
-    }
-}
-
-}   
+// 用ArrayList,Set,Map中的方法,不能取随机元素。\n
+// 可以用HashSet中的方法,Set set = new HashSet(); set.add(new Integer(1));
+//        Integer[] ints = new Integer[] { 1, 2, 3, 4, 5 };
+//        System.out.println(set.size());
+//        set.addAll(Arrays.asList(ints));
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+///        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.printlntln(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//              System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+///        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.printlntln(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set.size());
+//        System.out.println(set<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
