8921fd97147591e9a24fba279f4c0b437a132888
Administrator <1019358770@qq.com>
2023-07-11 19:45:15 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,70 +21,38 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 用ArrayList,Set,Map中的方法,不能取随机元素。\n
-// 可以用HashSet中的方法,Set set = new HashSet(); set.add(new Integer(1));
-//        Integer[] ints = new Integer[] { 1, 2, 3, 4, 5 };
-//        System.out.println(set.size());
-//        set.addAll(Arrays.asList(ints));
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-///        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.printlntln(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//              System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-///        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.printlntln(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set.size());
-//        System.out.println(set<h1><p>Bad Request</p></h1>   
+// 使用Random.nextInt()方法可以随机返回int值，该值落在0到Integer.MAX_VALUE之间。但是这并不是一个均匀的分布，而是遵循高斯分布，即钟形曲线。\n
+// 该分布的特点是中间大，两头小。为了生成一个均匀分布的随机数，需要对结果进行取模。\n
+// 另外，使用Random.nextInt(int n)方法可以生成0到n之间的随机数，所以可以指定n来获取n以内的随机整数。\n
+// 最后，如果要生成0到1之间的随机数，可以使用Random.nextDouble()方法，它会返回一个double值，该值落在0到1之间。
+
+
+import java.util.Random;
+
+public class TestRandom {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        for (int i = 0; i < 10; i++) {
+            int randInt = rand.nextInt(100);
+            System.out.println(randInt);
+        }
+
+        System.out.println("---------------------------------------");
+
+        for (int i = 0; i < 10; i++) {
+            int randInt = rand.nextInt(10);
+            System.out.println(randInt);
+        }
+
+        System.out.println("---------------------------------------");
+
+        for (int i = 0; i < 10; i++) {
+            double randDouble = rand.nextDouble();
+            System.out.println(randDouble);
+        }
+    }
+}```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n```\n<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
