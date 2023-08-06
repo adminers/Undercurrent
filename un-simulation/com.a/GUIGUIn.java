4f3757cea6bdea2445ae1c992bfdcc497b5e1008
Administrator <1019358770@qq.com>
2023-08-06 09:00:03 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,39 +21,8 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
-
-import java.awt.*;
-import javax.swing.*;
-
-public class Square extends JFrame {
-    public Square() {
-        setTitle("Square");
-        setSize(300, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(new FlowLayout());
-        JButton button = new JButton("Draw");
-        button.addActionListener(e -> {
-            for (int i = 0; i < 10; i++) {
-                JPanel panel = new JPanel();
-                panel.setBackground(Color.BLACK);
-                panel.setPreferredSize(new Dimension(100, 100));
-                add(panel);
-            }
-            repaint();
-        });
-        add(button);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new Square();
-            frame.setVisible(true);
-        });
-    }
-}
-   
+// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
+// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,47 +21,63 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 要求: 随机抽取的元素不能是重复的。\n
-// 提示: 可以使用LinkedHashSet集合,它是有序的,并且可以保证没有重复元素。\n
-// 注意: 该方法不是必须的,只是为了方便测试。
+// 要求: 随机取的元素不能是null, 并且不能重复。\n
+// 实现思路:
+// 1. 定义一个List集合, 存储随机取的元素。\n
+// 2. 定义一个Map集合, 存储集合中元素的索引。\n
+// 3. 定义一个随机数对象, 用于随机生成索引。\n
+// 4. 遍历集合, 判断集合中的元素是否为null, 并且判断集合中是否已经存在该元素。\n
+// 5. 如果集合中不存在该元素, 并且该元素不为null, 则将该元素添加到集合中, 并且将该元素在集合中的索引添加到Map集合中。\n
+// 6. 遍历Map集合, 随机生成索引, 并且判断该索引是否在Map集合中。\n
+// 7. 如果该索引在Map集合中, 则将该索引对应的元素添加到集合中。\n
+// 8. 遍历集合, 返回集合中的第一个元素。\n
+// 9. 返回集合中的第一个元素。\n
 
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class RandomElement {
     public static void main(String[] args) {
-        LinkedHashSet<Integer> set = new LinkedHashSet<>();
-        Random random = new Random();
-        for (int i = 0; i < 10; i++) {
-            set.add(random.nextInt(100));
-        }
-        System.out.println(set);
-        System.out.println(set.size());
-        System.out.println(set.iterator().next());
-    }
-}
-
-// 输出结果:
-// [78, 14, 73, 67, 23, 85, 52, 97, 90, 49]
-// 10
-// 78
-
-// 解析:
-// 首先创建一个LinkedHashSet集合,它是有序的,并且可以保证没有重复元素。
-// 然后创建一个Random对象,调用nextInt(100)方法,生成一个0~99的随机数,
-// 然后将随机数添加到LinkedHashSet集合中,最后使用迭代器遍历集合,并输出第一个元素。
-
-// 注意:
-// 1. 该方法不是必须的,只是为了方便测试。
-// 2. 创建一个LinkedHashSet集合,它是有序的,并且可以保证没有重复元素。
-// 3. 创建一个Random对象,调用nextInt(100)方法,生成一个0~99的随机数,
-// 4. 然后将随机数添加到LinkedHashSet集合中,最后使用迭代器遍历集合,并输出第一个元素。
-
-// 注意:
-// 1. 该方法不是必须的,只是为了方便测试。
-// 2. 创建一个LinkedHashSet集合,它是有序的,并且可以保证没有重复元素。
-// 3. 创建一个Random对象,调用nextInt(100)方法,生成一个0~99的随机数,
-// 4. 然后将随机数添加到LinkedHashSet集合中,最后使用迭代器遍历集合,并输出第一个元素。<h1><p>Bad Request</p></h1>   
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("h");
+        list.add("i");
+        list.add("j");
+        list.add("k");
+        list.add("l");
+        list.add("m");
+        list.add("n");
+        list.add("o");
+        list.add("p");
+        list.add("q");
+        list.add("r");
+        list.add("s");
+        list.add("t");
+        list.add("u");
+        list.add("v");
+        list.add("w");
+        list.add("x");
+        list.add("y");
+        list.add("z");
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+        list.add("G");
+        list.add("H");
+        list.add("I");
+        list.add("J");<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
