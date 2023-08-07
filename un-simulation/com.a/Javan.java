0c4dd4eb5040704f83fb230459e207fe431d84dd
Administrator <1019358770@qq.com>
2023-08-07 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,63 +21,27 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 要求: 随机取的元素不能是null, 并且不能重复。\n
-// 实现思路:
-// 1. 定义一个List集合, 存储随机取的元素。\n
-// 2. 定义一个Map集合, 存储集合中元素的索引。\n
-// 3. 定义一个随机数对象, 用于随机生成索引。\n
-// 4. 遍历集合, 判断集合中的元素是否为null, 并且判断集合中是否已经存在该元素。\n
-// 5. 如果集合中不存在该元素, 并且该元素不为null, 则将该元素添加到集合中, 并且将该元素在集合中的索引添加到Map集合中。\n
-// 6. 遍历Map集合, 随机生成索引, 并且判断该索引是否在Map集合中。\n
-// 7. 如果该索引在Map集合中, 则将该索引对应的元素添加到集合中。\n
-// 8. 遍历集合, 返回集合中的第一个元素。\n
-// 9. 返回集合中的第一个元素。\n
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
-public class RandomElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        list.add("d");
-        list.add("e");
-        list.add("f");
-        list.add("g");
-        list.add("h");
-        list.add("i");
-        list.add("j");
-        list.add("k");
-        list.add("l");
-        list.add("m");
-        list.add("n");
-        list.add("o");
-        list.add("p");
-        list.add("q");
-        list.add("r");
-        list.add("s");
-        list.add("t");
-        list.add("u");
-        list.add("v");
-        list.add("w");
-        list.add("x");
-        list.add("y");
-        list.add("z");
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
-        list.add("E");
-        list.add("F");
-        list.add("G");
-        list.add("H");
-        list.add("I");
-        list.add("J");<h1><p>Bad Request</p></h1>   
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n
+// 提示: 可以使用List集合中的方法。\n
+// 要求: 实现一个方法,随机取一个元素,要求不能使用随机数。\n<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
