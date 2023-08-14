d13e94563d1401c0d821f08bedacbe5da4a684f8
Administrator <1019358770@qq.com>
2023-08-14 10:07:29 +0800

    null Cache File Analysis.

---
+++ un-simulation/com.a/JavaListn.java
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Highlight.js Example</title>
+  <!-- 引入 highlight.js 样式文件 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/monokai.min.css">
+  <style>
+    /* 增加代码行之间的距离 */
+    pre {
+      line-height: 1.5em;
+    }
+  </style>
+  <!-- 引入 highlight.js 脚本文件 -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+</head>
+<body>
+
+  <h1>HTML 代码示例</h1>
+
+  <pre><code class="java">
+ // 在Java List中,随机取2个元素。\n
+
+// 要求：\n
+// 1. 随机性：每次运行程序，结果不一样。\n
+// 2. 去重性：不能取重复的元素。\n
+// 3. 取2个元素：取出的元素个数是2个。\n
+// 4. 取出的元素顺序和输入的顺序一致。\n
+// 5. 取出的元素不能是null。\n
+// 6. 取出的元素不能是数组。\n
+// 7. 取出的元素不能是List。\n
+// 8. 取出的元素不能是Map。\n
+// 9. 取出的元素不能是Set。\n
+// 10. 取出的元素不能是String。\n
+// 11. 取出的元素不能是Integer。\n
+// 12. 取出的元素不能是Double。\n
+// 13. 取出的元素不能是Float。\n
+// 14. 取出的元素不能是Long。\n
+// 15. 取出的元素不能是Short。\n
+// 16. 取出的元素不能是Byte。\n
+// 17. 取出的元素不能是Boolean。\n
+// 18. 取出的元素不能是Date。\n
+// 19. 取出的元素不能是File。\n
+// 20. 取出的元素不能是URL。\n
+// 21. 取出的元素不能是URI。\n
+// 22. 取出的元素不能是UUID。\n
+// 23. 取出的元素不能是BigInteger。\n
+// 24. 取出的元素不能是BigDecimal。\n
+// 25. 取出的元素不能是Math.random()。\n
+// 26. 取出的元素不能是Math.PI。\n
+// 27. 取出的元素不能是Math.E。\n
+// 28. 取出的元素不能是Math.sqrt(2)。\n
+// 29. 取出的元素不能是Math.abs(1)。\n
+// 30. 取出的元素不能是Math.max(1,2)。\n
+// 31. 取出的元素不能是Math.min(1,2)。\n
+// 32. 取出的元素不能是Math.pow(1,2)。\n
+// 33. 取出的元素不能是Math.log(1)。\n
+// 34. 取出的元素不能是Math.log10(1)。\n
+// 35. 取出的元素不能是Math.exp(1)。\n<h1><p>Bad Request</p></h1>   
+  </code></pre>
+
+</body>
+</html>
