e587c6a43bde187eb98102fb53925ea8a24b4c72
Administrator <1019358770@qq.com>
2023-08-16 09:03:11 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,8 +21,84 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 要求: 1. 随机性, 2. 取出的元素不能是重复的。\n
-// 思路: 1. 利用HashSet的特性, 2. 利用HashSet的特性, 3. 利用HashSet的特性, 4. 利用HashSet的特性, 5. 利用HashSet的特性, 6. 利用HashSet的特性, 7. 利用HashSet的特性, 8. 利用HashSet的特性, 9. 利用HashSet的特性, 10. 利用HashSet的特性, 11. 利用HashSet的特性, 12. 利用HashSet的特性, 13. 利用HashSet的特性, 14. 利用HashSet的特性, 15. 利用HashSet的特性, 16. 利用HashSet的特性, 17. 利用HashSet的特性, 18. 利用HashSet的特性, 19. 利用HashSet的特性, 20. 利用HashSet的特性, 21. 利用HashSet的特性, 22. 利用HashSet的特性, 23. 利用HashSet的特性, 24. 利用HashSet的特性, 25. 利用HashSet的特性, 26. 利用HashSet的特性, 27. 利用HashSet的特性, 28. 利用HashSet的特性, 29. 利用HashSet的特性, 30. 利用HashSet的特性, 31. 利用HashSet的特性, 32. 利用HashSet的特性, 33. 利用HashSet的特性, 34. 利用HashSet的特性, 35. 利用HashSet的特性, 36. 利用HashSet的特性, 37. 利用HashSet的特性, 38. 利用HashSet的特性, 39. 利用HashSet的特性, 40. 利用HashSet的特性, 41. 利用HashSet的特性, 42. 利用HashSet的特性, 43. 利用HashSet的特性, 44. 利用HashSet的特性, 45. 利用HashSet的特性, 46. 利用HashSet的特性, 47. 利用HashSet的特性, 48. 利用HashSet的特性, 49. 利用HashSet的特性, 50. 利用HashSet的特性, 51. 利用HashSet的特性, 52. 利用HashSet的特性, 53. 利用HashSet的特性, 54. 利用HashSet的特性, 55. 利用HashSet的特性, 56. 利用HashSet的特性, 57. 利用HashSet的特性, 58. 利用HashSet的特性, 59. 利用HashSet的特性, 60. 利用HashSet的特性, 61. 利用HashSet的特性, 62. 利用HashSet的特性, 63. 利用HashSet的特性, 64. 利用HashSet的特性, 65.<h1><p>Bad Request</p></h1>   
+// 在Java8中,可以使用Stream API中的random方法来实现。\n
+// 例如:
+// List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+// int randomNum = list.stream().random().get();
+// System.out.println(randomNum);
+// 输出结果:
+// 4
+// 
+// 要求:
+// 请实现一个方法,可以随机取一个集合中的元素。
+// 
+// 示例:
+// List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+// int randomNum = getRandomElement(list);
+// System.out.println(randomNum);
+// 输出结果:
+// 4
+// 
+// 提示:
+// 集合中元素的个数小于等于1000000000
+// 
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        int randomNum = getRandomElement(list);
+        System.out.println(randomNum);
+    }
+
+    public static int getRandomElement(List<Integer> list) {
+        Random random = new Random();
+        int randomNum = random.nextInt(list.size());
+        return list.get(randomNum);
+    }
+}
+
+
+
+// 方法二:
+// 思路:
+// 1. 获取集合的长度
+// 2. 生成一个随机数, 0 <= randomNum < list.size()
+// 3. 返回集合中对应的随机数
+// 
+// 复杂度分析:
+// 时间复杂度: O(1)
+// 空间复杂度: O(1)
+// 
+// 方法三:
+// 思路:
+// 1. 获取集合的长度
+// 2. 生成一个随机数, 0 <= randomNum < list.size()
+// 3. 返回集合中对应的随机数
+// 
+// 复杂度分析:
+// 时间复杂度: O(1)
+// 空间复杂度: O(1)
+// 
+// 方法四:
+// 思路:
+// 1. 获取集合的长度
+// 2. 生成一个随机数, 0 <= randomNum < list.size()
+// 3. 返回集合中对应的随机数
+// 
+// 复杂度分析:
+// 时间复杂度: O(1)
+// 空间复杂度: O(1)
+// 
+// 方法五:
+// 思路:
+// 1. 获取集合的长度
+// 2. 生成一个随机数, 0 <= randomNum < list.size()
+// 3. 返回集合中对应的随机数<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
