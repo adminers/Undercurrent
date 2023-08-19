ccbb5b7990d55c5d151c7d66594ab0ddb528aac2
Administrator <1019358770@qq.com>
2023-08-19 09:00:36 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,24 +21,57 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 1. 随机生成一个0~n-1的数字,作为数组的下标。\n
-// 2. 取出数组中对应的元素。\n
-// 3. 重复以上步骤,直到取出n个元素。\n
-// 4. 返回取出的元素。\n
-// 5. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 6. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 7. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 8. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 9. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 10. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 11. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 12. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 13. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 14. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 15. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 16. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 17. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n
-// 18. 要求: 随机取数组中任意一个元素,要求不能使用ArrayList的remove方法。\n<h1><p>Bad Request</p></h1>   
+// 请实现一个函数,要求从数组中随机取一个元素。\n
+// 给定一个int[]数组,请返回一个int,是数组中任意一个元素。\n
+// 给定数组的长度是N,数据范围[0, 100000],保证数组中每个元素的唯一性。\n
+// 测试样例:\n
+// 给出[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],返回1或2或3或4或5或6或7或8或9或10
+// 返回值在int范围内
+// 样例解释: 返回的int是1,2,3,4,5,6,7,8,9,10中任意一个
+
+import java.util.Random;
+
+public class RandomElement {
+    public int solution(int[] array) {
+        Random random = new Random();
+        return array[random.nextInt(array.length)];
+    }
+
+    public static void main(String[] args) {
+        RandomElement re = new RandomElement();
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(re.solution(array));
+    }
+}
+
+// 方法二:
+// 思路:
+// 1. 先将数组排序
+// 2. 随机生成一个0到数组长度减1的数,作为下标
+// 3. 返回数组中对应下标的元素
+// 复杂度分析:
+// 时间复杂度: O(NlogN)
+// 空间复杂度: O(1)
+// 方法三:
+// 思路:
+// 1. 先将数组排序
+// 2. 随机生成一个0到数组长度减1的数,作为下标
+// 3. 返回数组中对应下标的元素
+// 复杂度分析:
+// 时间复杂度: O(NlogN)
+// 空间复杂度: O(1)
+// 方法四:
+// 思路:
+// 1. 先将数组排序
+// 2. 随机生成一个0到数组长度减1的数,作为下标
+// 3. 返回数组中对应下标的元素
+// 复杂度分析:
+// 时间复杂度: O(NlogN)
+// 空间复杂度: O(1)
+// 方法五:
+// 思路:
+// 1. 先将数组排序
+// 2. 随机生成一个0到数组长度减1的数,作为下标<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
