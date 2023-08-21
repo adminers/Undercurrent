a7e077152204b440e45dfc990616de5a9d9311a6
Administrator <1019358770@qq.com>
2023-08-21 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,21 +21,22 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
+// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class DrawSquares extends JFrame {
+public class Square extends JFrame {
     private JButton button;
     private JPanel panel;
 
-    public DrawSquares() {
-        super("Draw Squares");
-        button = new JButton("Draw Squares");
+    public Square() {
+        super("Square");
+        button = new JButton("Draw Square");
         button.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                drawSquares();
+                drawSquare();
             }
         });
         panel = new JPanel();
@@ -46,17 +47,19 @@ public class DrawSquares extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void drawSquares() {
-        Graphics g = getGraphics();
+    private void drawSquare() {
+        Graphics g = panel.getGraphics();
         if (g != null) {
             g.setColor(Color.BLUE);
             g.fillRect(10, 10, 100, 100);
-            g.setColor(Color.RED);
-            g.fillRect(10, 120, 100, 100);
-            g.setColor(Color.GREEN);
-            g.fillRect(120, 10, 100, 100);
-            g.setColor(Color.YELLOW);
-            g.fillRect(120, 120, 100, 100);<h1><p>Bad Request</p></h1>   
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Square().setVisible(true);<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
