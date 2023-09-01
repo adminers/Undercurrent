bc15051b4cb38fef0f340bd58836152a24246976
Administrator <1019358770@qq.com>
2023-09-01 09:00:36 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,48 +21,22 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 要求: 随机取的元素不能是null, 并且不能重复。\n
-// 实现思路: 利用LinkedHashSet的特性, 随机取一个元素, 并且不能重复。
-
-import java.util.LinkedHashSet;
-import java.util.Random;
-
-public class RandomElement {
-    public static void main(String[] args) {
-        RandomElement randomElement = new RandomElement();
-        System.out.println(randomElement.randomElement(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-    }
-
-    public int randomElement(int[] nums) {
-        LinkedHashSet<Integer> set = new LinkedHashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-        Random random = new Random();
-        int index = random.nextInt(set.size());
-        int i = 0;
-        for (int num : set) {
-            if (i == index) {
-                return num;
-            }
-            i++;
-        }
-        return -1;
-    }
-}
-
-// 解题思路: 利用LinkedHashSet的特性, 随机取一个元素, 并且不能重复。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。
-// LinkedHashSet的特性: 存储的元素顺序和取出元素的顺序一致。<h1><p>Bad Request</p></h1>   
+// 要求: 
+// 1. 随机性要求高, 不要使用ArrayList的get(int index)方法, 因为该方法是顺序遍历的。\n
+// 2. 不要使用HashSet, 因为HashSet的底层是HashMap, 随机取一个元素, 需要先计算hash, 然后根据hash值找到对应的桶, 然后从桶中取元素, 这样效率就低了。\n
+// 3. 不要使用LinkedList, 因为LinkedList的随机取元素, 需要先遍历一遍, 然后取元素。\n
+// 4. 不要使用TreeSet, 因为TreeSet的底层是TreeMap, 随机取一个元素, 需要先计算hash, 然后根据hash值找到对应的桶, 然后从桶中取元素, 这样效率就低了。\n
+// 5. 不要使用ArrayList的remove(int index)方法, 因为该方法是顺序遍历的。\n
+// 6. 不要使用HashMap的remove(K key)方法, 因为该方法是顺序遍历的。\n
+// 7. 不要使用LinkedList的remove(int index)方法, 因为该方法是顺序遍历的。\n
+// 8. 不要使用TreeMap的remove(K key)方法, 因为该方法是顺序遍历的。\n
+// 9. 不要使用TreeMap的firstKey()方法, 因为该方法是顺序遍历的。\n
+// 10. 不要使用TreeMap的lastKey()方法, 因为该方法是顺序遍历的。\n
+// 11. 不要使用TreeMap的lowerKey(K key)方法, 因为该方法是顺序遍历的。\n
+// 12. 不要使用TreeMap的higherKey(K key)方法, 因为该方法是顺序遍历的。\n
+// 13. 不要使用TreeMap的floorKey(K key)方法, 因为该方法是顺序遍历的。\n
+// 14. 不要使用TreeMap的ceilingKey(K key)方法, 因为该方法是顺序遍历的。\n
+// 15. 不要使用TreeMap的subMap(K fromKey, K toKey)方法, 因为该方法是顺序遍<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
