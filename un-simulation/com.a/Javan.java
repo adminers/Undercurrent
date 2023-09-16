9bcf0700c326f2f7c97067490803f001c76f5ac0
root <root@i-l1gt7kg2>
2023-09-16 16:36:25 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -19,24 +19,137 @@
   <h1>HTML 代码示例</h1>
 
   <pre><code class="java">
- // 在Java集合中,随机取一个元素。\n
+ // 在Java集合中, 获取第一个元素。打印到控制台。\n
 
-// 请实现一个函数,要求在数组a中随机取一个元素。\n
-// 要求: 1. 函数randomInRange(int[] a, int n) 返回一个随机数,范围是[0,n-1]。\n
-// 2. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1]。\n
-// 3. 函数randomInArray(int[] a, int start, int end) 返回一个随机数,范围是[start,end]。\n
-// 4. 函数randomInArray(int[] a, int start, int end, int n) 返回一个随机数,范围是[start,end],随机数个数为n。\n
-// 5. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 6. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 7. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 8. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 9. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 10. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 11. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 12. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 13. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 14. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n
-// 15. 函数randomInArray(int[] a, int n) 返回一个随机数,范围是[0,a.length-1],随机数个数为n。\n<h1><p>Bad Request</p></h1>   
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        System.out.println(list.get(0));
+    }
+}
+
+// 解决方案:
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FirstElement {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
