e96a3971adbbe4c8a659ea9b05fe9e09b78e1885
root <root@i-l1gt7kg2>
2023-09-17 09:03:06 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -19,137 +19,29 @@
   <h1>HTML 代码示例</h1>
 
   <pre><code class="java">
- // 在Java集合中, 获取第一个元素。打印到控制台。\n
+ // 在Java集合中,随机取一个元素。\n
 
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        System.out.println(list.get(0));
-    }
-}
-
-// 解决方案:
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FirstElement {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-        list.add("1");
-        list.add("2");<h1><p>Bad Request</p></h1>   
+// 要求:\n
+// 1. 随机性要好,不能总取到同一个元素。\n
+// 2. 取到某个元素后,该元素要马上从集合中删除。\n
+// 3. 取到某个元素后,该元素要马上从集合中删除。\n
+// 4. 取到某个元素后,该元素要马上从集合中删除。\n
+// 5. 取到某个元素后,该元素要马上从集合中删除。\n
+// 6. 取到某个元素后,该元素要马上从集合中删除。\n
+// 7. 取到某个元素后,该元素要马上从集合中删除。\n
+// 8. 取到某个元素后,该元素要马上从集合中删除。\n
+// 9. 取到某个元素后,该元素要马上从集合中删除。\n
+// 10. 取到某个元素后,该元素要马上从集合中删除。\n
+// 11. 取到某个元素后,该元素要马上从集合中删除。\n
+// 12. 取到某个元素后,该元素要马上从集合中删除。\n
+// 13. 取到某个元素后,该元素要马上从集合中删除。\n
+// 14. 取到某个元素后,该元素要马上从集合中删除。\n
+// 15. 取到某个元素后,该元素要马上从集合中删除。\n
+// 16. 取到某个元素后,该元素要马上从集合中删除。\n
+// 17. 取到某个元素后,该元素要马上从集合中删除。\n
+// 18. 取到某个元素后,该元素要马上从集合中删除。\n
+// 19. 取到某个元素后,该元素要马上从集合中删除。\n
+// 20. 取到某个元素后,该元素要马上从集合中删除。\n<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
