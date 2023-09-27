a11ceedc1949d0ff4142cf7808238ad44a66029d
root <root@i-l1gt7kg2>
2023-09-27 09:09:41 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,8 +21,43 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n<h1><p>Bad Request</p></h1>   
+// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
+import java.awt.*;
+import javax.swing.*;
+
+public class Square extends JFrame {
+    public Square() {
+        super("Square");
+        setLayout(new FlowLayout());
+        JButton b = new JButton("Draw");
+        add(b);
+        b.addActionListener(e -> {
+            JPanel p = new JPanel() {
+                @Override
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.setColor(Color.BLACK);
+                    g.fillRect(0, 0, getWidth(), getHeight());
+                }
+            };
+            p.setPreferredSize(new Dimension(100, 100));
+            add(p);
+            pack();
+            setVisible(true);
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Square::new);
+    }
+}
+
+/* Output:
+
+*/
+
+
+   
   </code></pre>
 
 </body>
