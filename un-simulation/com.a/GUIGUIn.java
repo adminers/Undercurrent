fd094159bcff600dbed426ad8d167a8f61885479
root <root@i-l1gt7kg2>
2023-10-12 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,48 +21,60 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.Pane;
+// import javafx.scene.paint.Color;
+// import javafx.scene.shape.Rectangle;
+// import javafx.stage.Stage;
 
-public class Square extends JFrame {
-    private JButton button;
-    private JPanel panel;
-
-    public Square() {
-        super("Square");
-        setLayout(new FlowLayout());
-        button = new JButton("Draw");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                drawSquare();
-            }
+public class Square extends Application {
+    public void start(Stage stage) {
+        Pane pane = new Pane();
+        pane.setPrefSize(400, 400);
+        pane.setStyle("-fx-background-color: black;");
+        pane.setOnMouseClicked(e -> {
+            Rectangle rect = new Rectangle(100, 100);
+            rect.setFill(Color.RED);
+            pane.getChildren().add(rect);
         });
-        add(button);
-        panel = new JPanel();
-        panel.setBackground(Color.BLACK);
-        add(panel);
-        pack();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-
-    public void drawSquare() {
-        int width = panel.getWidth();
-        int height = panel.getHeight();
-        panel.setPreferredSize(new Dimension(width, height));
-        panel.setBackground(Color.BLACK);
-        panel.repaint();
+        stage.setScene(new Scene(pane));
+        stage.show();
     }
 
     public static void main(String[] args) {
-        Square frame = new Square();
+        launch(args);
     }
 }
 
-class Square extends JFrame {<h1><p>Bad Request</p></h1>   
+// 圆形,例如使用圆形的拼写检查方法来检查圆形的拼写是否正确,并根据检查结果调整圆形的半径和颜色。
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.Pane;
+// import javafx.scene.paint.Color;
+// import javafx.scene.shape.Circle;
+// import javafx.stage.Stage;
+
+class Circle extends Application {
+    public void start(Stage stage) {
+        Pane pane = new Pane();
+        pane.setPrefSize(400, 400);
+        pane.setStyle("-fx-background-color: black;");
+        pane.setOnMouseClicked(e -> {
+            Circle circle = new Circle(100, 100, 50);
+            circle.setFill(Color.RED);
+            pane.getChildren().add(circle);
+        });
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+// 椭圆,例如使用椭圆形的拼写检查方法来检查椭圆形的拼写是否正确,并根据检查结果调整椭圆<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
