c830cee0afbecd5982bcefc740d33d5926e0390a
root <root@i-l1gt7kg2>
2023-10-26 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -22,7 +22,37 @@
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
 // 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n<h1><p>Bad Request</p></h1>   
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Square extends JFrame {
+    private JButton button;
+    private JPanel panel;
+
+    public Square() {
+        super("Square");
+        setLayout(new BorderLayout());
+        panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        button = new JButton("Draw");
+        button.addActionListener(new ButtonListener());
+        add(panel, BorderLayout.CENTER);
+        add(button, BorderLayout.SOUTH);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            panel.removeAll();
+            panel.setLayout(new GridLayout(2, 2));
+            panel.add(new JButton("Button 1"));
+            panel.add(new JButton("Button 2"));
+            panel.add(new JButton("Button 3"));
+            panel.add(new JButton("Button 4"));
+            panel.revalidate();<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
