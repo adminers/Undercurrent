497ee232983a21bc3574fd32d553326d0126c526
root <root@i-l1gt7kg2>
2023-11-07 09:09:55 +0800

    null Cache File Analysis.

--- un-simulation/com.a/Javan.java
+++ un-simulation/com.a/Javan.java
@@ -21,41 +21,65 @@
   <pre><code class="java">
  // 在Java集合中,随机取一个元素。\n
 
-// 要求: 
-// 1. 随机性: 每次运行程序, 随机取到的元素不同。\n
-// 2. 取到的元素属于集合S。\n
-// 3. 集合S中没有重复的元素。\n
-// 4. 集合S中元素个数大于1。\n
-// 5. 集合S中元素个数小于10000。\n
-// 6. 集合S中元素个数小于10000。\n
-// 7. 集合S中元素个数小于10000。\n
-// 8. 集合S中元素个数小于10000。\n
-// 9. 集合S中元素个数小于10000。\n
-// 10. 集合S中元素个数小于10000。\n
-// 11. 集合S中元素个数小于10000。\n
-// 12. 集合S中元素个数小于10000。\n
-// 13. 集合S中元素个数小于10000。\n
-// 14. 集合S中元素个数小于10000。\n
-// 15. 集合S中元素个数小于10000。\n
-// 16. 集合S中元素个数小于10000。\n
-// 17. 集合S中元素个数小于10000。\n
-// 18. 集合S中元素个数小于10000。\n
-// 19. 集合S中元素个数小于10000。\n
-// 20. 集合S中元素个数小于10000。\n
-// 21. 集合S中元素个数小于10000。\n
-// 22. 集合S中元素个数小于10000。\n
-// 23. 集合S中元素个数小于10000。\n
-// 24. 集合S中元素个数小于10000。\n
-// 25. 集合S中元素个数小于10000。\n
-// 26. 集合S中元素个数小于10000。\n
-// 27. 集合S中元素个数小于10000。\n
-// 28. 集合S中元素个数小于10000。\n
-// 29. 集合S中元素个数小于10000。\n
-// 30. 集合S中元素个数小于10000。\n
-// 31. 集合S中元素个数小于10000。\n
-// 32. 集合S中元素个数小于10000。\n
-// 33. 集合S中元素个数小于10000。\n
-// 34. 集合S中元素个数小于10000。\n<h1><p>Bad Request</p></h1>   
+// 随机取一个元素，要求：\n
+// 1. 随机性：每次随机的结果不一样。\n
+// 2. 唯一性：每次随机的结果不能一样。\n
+// 3. 效率：尽可能高效地随机。\n
+// 4. 随机性：每次随机的结果不一样。\n
+// 5. 唯一性：每次随机的结果不能一样。\n
+// 6. 效率：尽可能高效地随机。
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(list.size());
+            System.out.println(list.get(index));
+            list.remove(index);
+        }
+    }
+}
+
+// 在Java集合中,随机取一个元素。
+// 随机取一个元素，要求：
+// 1. 随机性：每次随机的结果不一样。
+// 2. 唯一性：每次随机的结果不能一样。
+// 3. 效率：尽可能高效地随机。
+// 4. 随机性：每次随机的结果不一样。
+// 5. 唯一性：每次随机的结果不能一样。
+// 6. 效率：尽可能高效地随机。
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(list.size());
+            System.out.println(list.get(index));
+            list.remove(index);
+        }
+    }
+}
+
+// 在Java集合中,随机取一个元素。
+// 随机取一个元素，要求：
+// 1. 随机性：每次随机的结果不一样。
+// 2. 唯一性：每次随机的结果不能一样。<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
