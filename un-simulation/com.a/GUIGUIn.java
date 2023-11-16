e8131ff59812d315f5d80a9367c21ecfaa513736
root <root@i-l1gt7kg2>
2023-11-16 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -22,102 +22,42 @@
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
 // 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Square extends JFrame {
-    public Square() {
-        super("Square");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(1, 1));
-        JButton b = new JButton("Draw");
-        add(b);
-        b.addActionListener((e) -> {
-            JPanel p = new JPanel() {
-                @Override
-                protected void paintComponent(Graphics g) {
-                    super.paintComponent(g);
-                    g.setColor(Color.BLACK);
-                    g.fillRect(0, 0, getWidth(), getHeight());
-                }
-            };
-            p.setPreferredSize(new Dimension(100, 100));
-            add(p);
-            pack();
-            repaint();
-        });
-        pack();
-        setVisible(true);
+public class Square extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        Square frame = new Square("Square");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Square();
+    public Square(String title) {
+        super(title);
+        setLayout(new FlowLayout());
+        JButton button = new JButton("Draw Square");
+        button.addActionListener(this);
+        add(button);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        JButton button = (JButton) event.getSource();
+        if (button.getText().equals("Draw Square")) {
+            button.setText("Erase Square");
+            setBackground(Color.RED);
+        } else {
+            button.setText("Draw Square");
+            setBackground(Color.WHITE);
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<h1><p>Bad Request</p></h1>   
+class Square extends JPanel implements ActionListener {
+    public Square(String title) {
+        super(title);
+        setLayout(new FlowLayout());<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
