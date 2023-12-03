9f91db8c64e7553ec69adceb9dd5495aa4fbb247
root <root@i-l1gt7kg2>
2023-12-03 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,51 +21,284 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// import javax.swing.*;\n
-// import java.awt.*;\n
-// import java.awt.event.*;\n
-// import java.util.*;\n
-// import java.awt.geom.*;\n
-// import java.awt.image.*;\n
-// import java.io.*;\n
-// import java.net.*;\n
-// import java.lang.*;\n
-// import java.lang.reflect.*;\n
-// import java.util.concurrent.*;\n
-// import java.util.concurrent.locks.*;\n
-// import java.util.concurrent.atomic.*;\n
-// import java.util.concurrent.locks.Lock;\n
-// import java.util.concurrent.locks.ReentrantLock;\n
-// import java.util.concurrent.locks.ReentrantReadWriteLock;\n
-// import java.util.concurrent.locks.ReentrantLock;\n
-// import java.util.concurrent.locks.ReentrantReadWriteLock;\n
-// import java.util.concurrent.atomic.*;\n
-// import java.util.concurrent.atomic.AtomicInteger;\n
-// import java.util.concurrent.atomic.AtomicLong;\n
-// import java.util.concurrent.atomic.AtomicBoolean;\n
-// import java.util.concurrent.atomic.AtomicIntegerArray;\n
-// import java.util.concurrent.atomic.AtomicLongArray;\n
-// import java.util.concurrent.atomic.AtomicReference;\n
-// import java.util.concurrent.atomic.AtomicReferenceArray;\n
-// import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;\n
-// import java.util.concurrent.atomic.AtomicLongFieldUpdater;\n
-// import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;\n
-// import java.util.concurrent.atomic.AtomicMarkableReference;\n
-// import java.util.concurrent.atomic.AtomicStampedReference;\n
-// import java.util.concurrent.locks.*;\n
-// import java.util.concurrent.locks.LockSupport;\n
-// import java.util.concurrent.locks.AbstractQueuedSynchronizer;\n
-// import java.util.concurrent.locks.ReentrantLock;\n
-// import java.util.concurrent.locks.ReentrantReadWriteLock;\n
-// import java.util.concurrent.locks.StampedLock;\n
-// import java.util.concurrent.locks.Lock;\n
-// import java.util.concurrent.locks.ReadWriteLock;\n
-// import java.util.concurrent.locks.Condition;\n
-// import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;\n
-// import java.util.concurrent.locks.AbstractQueuedSynchronizer;\n
-// import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;\n
-// import java.util.concurrent.locks.AbstractQueuedSynchronizer;\n
-// import java.util.concurrent.locks.AbstractQueuedSynchronizer;\n<h1><p>Bad Request</p></h1>   
+// import javafx.application.Application;
+// import javafx.scene.Scene;
+// import javafx.scene.layout.Pane;
+// import javafx.scene.paint.Color;
+// import javafx.scene.shape.Rectangle;
+// import javafx.stage.Stage;
+// import javafx.scene.control.Button;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
+// public class RectangleExample extends Application {
+//     @Override
+//     public void start(Stage stage) {
+//         Pane pane = new Pane();
+//         Rectangle rect = new Rectangle(100, 100, Color.BLACK);
+//         pane.getChildren().add(rect);
+//         Button button = new Button("Click me");
+//         button.setOnAction(new EventHandler<ActionEvent>() {
+//             @Override
+//             public void handle(ActionEvent event) {
+//                 if (rect.getWidth() == 100) {
+//                     rect.setWidth(200);
+//                 } else {
+//                     rect.setWidth(100);
+//                 }
+//             }
+//         });
+//         pane.getChildren().add(button);
+//         Scene scene = new Scene(pane, 200, 200);
+//         stage.setScene(scene);
+//         stage.show();
+//     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        Rectangle rect = new Rectangle(100, 100, Color.BLACK);
+        pane.getChildren().add(rect);
+        Scene scene = new Scene(pane, 200, 200);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
