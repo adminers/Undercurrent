314bfc90ffd961e932af44eb44b4b68d414dd08b
root <root@i-l1gt7kg2>
2023-12-10 09:00:01 +0800

    null Cache File Analysis.

--- un-simulation/com.a/GUIGUIn.java
+++ un-simulation/com.a/GUIGUIn.java
@@ -21,33 +21,42 @@
   <pre><code class="java">
  // 创建一个包含 10 个黑色的正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
 
-// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。\n
+// 创建一个包含 10 个黑色正方形的窗口。可以使用 GUI 组件库中的正方形组件来创建一个正方形窗口。在窗口中添加一个按钮,该按钮将引发一个事件处理程序,用于在单击按钮时绘制正方形。可以使用 GUI 事件处理程序的内置方法来绘制正方形,例如使用正方形的拼写检查方法来检查正方形的拼写是否正确,并根据检查结果调整正方形的大小和颜色。
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Square extends JFrame {
-    public Square() {
-        setTitle("Square");
+public class DrawSquares extends JFrame {
+    public DrawSquares() {
+        setTitle("Draw Squares");
         setSize(300, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(new GridLayout(1, 1));
-        JButton button = new JButton("Draw");
+        setLayout(new FlowLayout());
+        JButton button = new JButton("Draw Squares");
+        add(button);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JButton b = (JButton) e.getSource();
-                b.setText("");
-                b.setBackground(Color.BLACK);
-                b.setForeground(Color.WHITE);
-                b.setFont(new Font("Serif", Font.BOLD, 24));
-                b.setBorder(BorderFactory.createLineBorder(Color.WHITE, 3));
-                b.setOpaque(true);
-                b.setPreferredSize(new Dimension(300, 300));
-                b.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
-                b.setBackground(Color.BLACK);
-                b.setForeground(Color.WHITE);<h1><p>Bad Request</p></h1>   
+                JFrame frame = new JFrame("Draw Squares");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new FlowLayout());
+                JPanel panel = new JPanel();
+                panel.setBackground(Color.RED);
+                frame.add(panel);
+                frame.setSize(300, 300);
+                frame.setVisible(true);
+            }
+        });
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new DrawSquares();
+    }
+}
+
+class DrawSquares extends JFrame {<h1><p>Bad Request</p></h1>   
   </code></pre>
 
 </body>
