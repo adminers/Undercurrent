fac1470d98f7b78861917f62cbe992cc4a65640c
Administrator <1019358770@qq.com>
2024-02-26 10:49:19 +0800

    java create class;

---
+++ un-common/src/main/java/com/qiaweidata/work/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.qiaweidata.work;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("MyThread-");
+        executor.initialize();
+        return executor;
+    }
+}
