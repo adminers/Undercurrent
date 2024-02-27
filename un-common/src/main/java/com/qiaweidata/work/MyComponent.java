76775bd2a31ad91c9b578aa81b0588ab5ce52dd2
Administrator <1019358770@qq.com>
2024-02-27 09:27:01 +0800

    The time has come;  Only recently, with the support of a newer tycoon from the class of 1980, have the co-ed supporters on the board felt able to press their case;

---
+++ un-common/src/main/java/com/qiaweidata/work/MyComponent.java
@@ -0,0 +1,12 @@
+package com.qiaweidata.work;
+
+public class MyComponent {
+
+    private MyService myService;
+
+    public MyComponent(MyService myService) {
+        this.myService = myService;
+    }
+
+    // 其他方法
+}
---
+++ un-common/src/main/java/com/qiaweidata/work/MyService.java
@@ -0,0 +1,5 @@
+package com.qiaweidata.work;
+
+public class MyService {
+    // 一些逻辑
+}
