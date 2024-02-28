6e404fdf04d6d7fa91b993f67bced2d52ce83803
Administrator <1019358770@qq.com>
2024-02-28 09:51:08 +0800

    create slip char;

--- un-common/src/main/java/com/qiaweidata/work/MyService.java
+++ un-common/src/main/java/com/qiaweidata/work/MyService.java
@@ -2,4 +2,11 @@ package com.qiaweidata.work;
 
 public class MyService {
     // 一些逻辑
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10_000; i++) {
+            System.out.println("科目辅助余额表" + i + ",数据报告" + i + ",报表组" + i);
+        }
+    }
 }
