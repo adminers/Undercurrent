60744d70f9df2eb471f537630367ecdea55fabef
Administrator <1019358770@qq.com>
2024-03-04 17:59:27 +0800

    server ;

---
+++ un-server/src/main/java/com/qiaweidata/undercurrent/FileUploadServer.java
@@ -0,0 +1,59 @@
+package com.qiaweidata.undercurrent;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+/**
+ * @Description: 文件上传demo 服务端
+ * @Author: walking
+ * @Date: 2019年10月29日16:49:52
+ */
+public class FileUploadServer {
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast(new ObjectEncoder());
+                            ch.pipeline().addLast(new ObjectDecoder(Integer.MAX_VALUE, ClassResolvers.weakCachingConcurrentResolver(null))); // 最大长度
+                            ch.pipeline().addLast(new FileUploadServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(port).sync();
+            System.out.println("服务端启动....");
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8074;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            new FileUploadServer().bind(port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
