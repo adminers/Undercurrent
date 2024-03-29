80deb6a76fc2e2cab77ca18f2278bd7cca1429cd
Administrator <1019358770@qq.com>
2024-03-29 10:00:41 +0800

    static file;

---
+++ un-common/src/main/java/com/qiaweidata/util/Constant.java
@@ -0,0 +1,16 @@
+package com.qiaweidata.util;
+
+/**
+ * @Title: Constant
+ * @Description: Constant
+ * @Company: www.wrenchdata.com
+ * @author: shenshilong
+ * @date: 2023-09-12
+ * @version: V1.0
+ */
+public class Constant {
+
+    public static final String STRING_MESSAGE_PREFIX = "STRING:";
+
+    public static final String FILE_MESSAGE_PREFIX = "FILE:";
+}
