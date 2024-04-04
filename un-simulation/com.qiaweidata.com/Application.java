5e838f019a1ab35fbaf89c7a2513bf58789fd579
Administrator <1019358770@qq.com>
2024-04-04 08:55:04 +0800

    un-simulation/com.qiaweidata.com/Application.java /  file update.

---
+++ un-simulation/com.qiaweidata.com/Application.java
@@ -0,0 +1,17 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        log.info("###### SpringBootApplication ###### ");
+        SpringApplication.run(Application.class, args);
+    }
+}
