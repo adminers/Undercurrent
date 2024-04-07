8bdc59d48b9014b889e7f4a9baa9998d6cfac6da
Administrator <1019358770@qq.com>
2024-04-08 07:38:23 +0800

    un-simulation/com.qiaweidata.com/SpringContextUtils.java /  file update.

--- un-simulation/com.qiaweidata.com/SpringContextUtils.java
+++ un-simulation/com.qiaweidata.com/SpringContextUtils.java
@@ -1,41 +1,95 @@
-package com.fly.log.utils;
+package com.fly.core.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.servlet.ServletContext;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Spring Context 工具类
  */
+@Slf4j
 @Component
 public class SpringContextUtils implements ApplicationContextAware
 {
-    public static ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
+    
+    /**
+     * web服务器基准URL
+     */
+    private static String SERVER_BASE_URL = null;
     
     @Override
     public void setApplicationContext(ApplicationContext applicationContext)
         throws BeansException
     {
+        log.info("###### execute setApplicationContext ######");
         SpringContextUtils.applicationContext = applicationContext;
+    }
+    
+    public static ApplicationContext getApplicationContext()
+    {
+        return applicationContext;
     }
     
     public static <T> T getBean(Class<T> clazz)
     {
+        Assert.notNull(applicationContext, "applicationContext is null");
         return applicationContext.getBean(clazz);
     }
     
     /**
-     * getActiveProfile
+     * execute @PostConstruct May be SpringContextUtils not inited, throw NullPointerException
      * 
      * @return
-     * @see [类、类#方法、类#成员]
      */
     public static String getActiveProfile()
     {
+        Assert.notNull(applicationContext, "applicationContext is null");
         String[] profiles = applicationContext.getEnvironment().getActiveProfiles();
         return (profiles != null && profiles.length > 0) ? StringUtils.join(profiles, ",") : "default";
+    }
+    
+    /**
+     * can use in @PostConstruct
+     * 
+     * @param context
+     * @return
+     */
+    public static String getActiveProfile(ApplicationContext context)
+    {
+        Assert.notNull(context, "context is null");
+        String[] profiles = context.getEnvironment().getActiveProfiles();
+        return (profiles != null && profiles.length > 0) ? StringUtils.join(profiles, ",") : "default";
+    }
+    
+    /**
+     * get web服务基准地址，一般为 http://${ip}:${port}/${contentPath}
+     * 
+     * @return
+     * @throws UnknownHostException
+     * @see [类、类#方法、类#成员]
+     */
+    public static String getServerBaseURL()
+        throws UnknownHostException
+    {
+        if (SERVER_BASE_URL == null)
+        {
+            ServletContext servletContext = getBean(ServletContext.class);
+            Assert.notNull(servletContext, "servletContext is null");
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            SERVER_BASE_URL = "http://" + ip + ":" + getProperty("server.port") + servletContext.getContextPath();
+        }
+        return SERVER_BASE_URL;
     }
     
     /**
