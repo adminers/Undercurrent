7c0209f71e16aade5aaca7022170f407c1bc0bb6
Administrator <1019358770@qq.com>
2024-04-08 19:57:52 +0800

    un-simulation/com.qiaweidata.com/Knife4jConfig.java /  file update.

--- un-simulation/com.qiaweidata.com/Knife4jConfig.java
+++ un-simulation/com.qiaweidata.com/Knife4jConfig.java
@@ -3,53 +3,53 @@ package com.fly.core.config;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 
 import io.swagger.annotations.ApiOperation;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * knife4j
+ * Knife4jConfig
  *
- * @author jack
  */
 @Configuration
 @EnableKnife4j
-@EnableSwagger2WebMvc
+@EnableSwagger2
 @ConditionalOnWebApplication
-@Import(BeanValidatorPluginsConfiguration.class)
 public class Knife4jConfig
 {
-    
-    @Value("${knife4j.enable: true}")
-    private boolean enable;
-    
-    @Bean(value = "defaultApi2")
-    public Docket defaultApi2()
+    /**
+     * 开发、测试环境接口文档打开
+     * 
+     * @return
+     * @see [类、类#方法、类#成员]
+     */
+    @Bean
+    public Docket createRestApi()
     {
-        return new Docket(DocumentationType.SWAGGER_2).enable(enable)
-            .apiInfo(new ApiInfoBuilder().title("接口API").description("接口文档").termsOfServiceUrl("http://00fly.online/").contact(new Contact("xx", "xx", "xx")).version("1.0").build())
-            // 分组名称
-            .groupName("0.1版")
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+            .enable(true)
             .select()
             .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-            .paths(PathSelectors.any())
+            .paths(PathSelectors.any()) // 包下的类，生成接口文档
             .build()
             .securitySchemes(security());
+    }
+    
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfoBuilder().title("数据接口API").description("接口文档").termsOfServiceUrl("http://00fly.online/").version("1.0.0").build();
     }
     
     private List<ApiKey> security()
