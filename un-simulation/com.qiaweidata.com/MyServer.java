80a343f5ba43c5ab4775212ec9db81bdc6809fd0
Administrator <1019358770@qq.com>
2024-05-07 13:14:28 +0800

    un-simulation/com.qiaweidata.com/MyServer.java /  file update.

--- un-simulation/com.qiaweidata.com/MyServer.java
+++ un-simulation/com.qiaweidata.com/MyServer.java
@@ -1,98 +1,11 @@
-package com.fly.socket.sample5;
+package com.fly.socket.sample8;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.net.ServerSocketFactory;
-import javax.net.ssl.SSLServerSocketFactory;
-
-public class MyServer
+public interface MyServer
 {
     
-    private final static Logger logger = Logger.getLogger(MyServer.class.getName());
+    public void startup()
+        throws Exception;
     
-    public static void main(String[] args)
-    {
-        try
-        {
-            ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
-            ServerSocket server = factory.createServerSocket(10000);
-            
-            while (true)
-            {
-                Socket socket = server.accept();
-                invoke(socket);
-            }
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-    }
-    
-    private static void invoke(final Socket socket)
-        throws IOException
-    {
-        new Thread(new Runnable()
-        {
-            public void run()
-            {
-                ObjectInputStream is = null;
-                ObjectOutputStream os = null;
-                try
-                {
-                    is = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-                    os = new ObjectOutputStream(socket.getOutputStream());
-                    
-                    Object obj = is.readObject();
-                    User user = (User)obj;
-                    System.out.println("user: " + user.getName() + "/" + user.getPassword());
-                    
-                    user.setName(user.getName() + "_new");
-                    user.setPassword(user.getPassword() + "_new");
-                    
-                    os.writeObject(user);
-                    os.flush();
-                }
-                catch (IOException ex)
-                {
-                    logger.log(Level.SEVERE, null, ex);
-                }
-                catch (ClassNotFoundException ex)
-                {
-                    logger.log(Level.SEVERE, null, ex);
-                }
-                finally
-                {
-                    try
-                    {
-                        is.close();
-                    }
-                    catch (Exception ex)
-                    {
-                    }
-                    try
-                    {
-                        os.close();
-                    }
-                    catch (Exception ex)
-                    {
-                    }
-                    try
-                    {
-                        socket.close();
-                    }
-                    catch (Exception ex)
-                    {
-                    }
-                }
-            }
-        }).start();
-    }
+    public void shutdown()
+        throws Exception;
 }
