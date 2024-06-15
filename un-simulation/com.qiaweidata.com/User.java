fb42662d3fa96369b6c4dff661de60e7d01237d9
Administrator <1019358770@qq.com>
2024-06-15 13:54:50 +0800

    un-simulation/com.qiaweidata.com/User.java /  file update.

--- un-simulation/com.qiaweidata.com/User.java
+++ un-simulation/com.qiaweidata.com/User.java
@@ -1,42 +1,57 @@
-package com.fly.socket.sample4;
+package com.fly.cache.model;
 
-public class User implements java.io.Serializable
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@ApiModel(description = "实体数据对象")
+public class User implements Serializable
 {
     private static final long serialVersionUID = 1L;
     
-    private String name;
-    
-    private String password;
-    
-    public User()
+    public interface Simple
     {
-        
     }
     
-    public User(String name, String password)
+    public interface All extends Simple
     {
-        this.name = name;
-        this.password = password;
     }
     
-    public String getName()
-    {
-        return name;
-    }
+    @JsonView(Simple.class)
+    @ApiModelProperty("用户id")
+    private Long userId;
     
-    public void setName(String name)
-    {
-        this.name = name;
-    }
+    @JsonView(Simple.class)
+    @NotBlank(message = "用户名不能为空")
+    @ApiModelProperty("用户名")
+    private String userName;
     
-    public String getPassword()
-    {
-        return password;
-    }
+    @JsonView(Simple.class)
+    @NotNull(message = "年龄不能为空")
+    @Range(min = 10, max = 60, message = "年龄必须在{min}-{max}")
+    @ApiModelProperty("年龄")
+    private Integer age;
     
-    public void setPassword(String password)
-    {
-        this.password = password;
-    }
+    @JsonView(All.class)
+    @ApiModelProperty("备注")
+    private String desc;
     
+    @JsonView(All.class)
+    @ApiModelProperty("创建时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
 }
