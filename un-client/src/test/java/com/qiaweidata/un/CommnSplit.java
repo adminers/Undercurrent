1245f0655ef90af7a4d4e28a3aa60387852d8c6c
Administrator <1019358770@qq.com>
2025-04-23 16:09:57 +0800

    create job;

---
+++ un-client/src/test/java/com/qiaweidata/un/CommnSplit.java
@@ -0,0 +1,20 @@
+package com.qiaweidata.un;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Title: CommnSplit
+ * @Description: CommnSplit
+ * @date: 2025-04-23
+ * @version: V1.0
+ */
+public class CommnSplit {
+
+    public static void main(String[] args) {
+
+        List<String> arr = Arrays.asList("a");
+        System.out.println(String.join( " and ", arr));
+    }
+}
