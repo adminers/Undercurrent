5637b9ff465076e123bb50bf5b9b1ba552bf0dfd
root <root@iZ0jl0q42o4i1mj8jt0c2cZ>
2025-07-21 18:00:03 +0800

    un-simulation/org/qiaweidata/vi.js /  file update.

--- un-simulation/org/qiaweidata/vi.js
+++ un-simulation/org/qiaweidata/vi.js
@@ -1,1 +1,16 @@
-﻿CKEDITOR.plugins.setLang("mathjax","vi",{title:"Toán học bằng TeX",button:"Toán",dialogInput:"Nhập mã TeX ở đây",docUrl:"http://en.wikibooks.org/wiki/LaTeX/Mathematics",docLabel:"Tài liệu TeX",loading:"đang nạp...",pathName:"toán"});
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'placeholder', 'vi',
+{
+	placeholder :
+	{
+		title		: 'Thuộc tính đặt chỗ',
+		toolbar		: 'Tạo đặt chỗ',
+		text		: 'Văn bản đặt chỗ',
+		edit		: 'Chỉnh sửa ',
+		textMissing	: 'The placeholder must contain text.' // MISSING
+	}
+});
