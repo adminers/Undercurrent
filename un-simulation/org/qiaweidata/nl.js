0d07149b58ac473ea860ac4dcd7f39296ec7a7ea
root <root@iZ0jl0q42o4i1mj8jt0c2cZ>
2025-08-04 12:00:01 +0800

    un-simulation/org/qiaweidata/nl.js /  file update.

--- un-simulation/org/qiaweidata/nl.js
+++ un-simulation/org/qiaweidata/nl.js
@@ -1,1 +1,15 @@
-﻿CKEDITOR.plugins.setLang("embedbase","nl",{pathName:"media object",title:"Mediareferentie",button:"Mediareferentie invoegen",unsupportedUrlGiven:"De opgegeven URL wordt niet ondersteund.",unsupportedUrl:"De URL {url} wordt niet ondersteund door Mediareferentie.",fetchingFailedGiven:"Kon de inhoud van de opgegeven URL niet ophalen",fetchingFailed:"Kon de inhoud van {url} niet ophalen.",fetchingOne:"Ophalen van oEmbed antwoord…",fetchingMany:"Ophalen van oEmbed antwoorden, {current} van {max} klaar…"});
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'uicolor', 'nl',
+{
+	uicolor :
+	{
+		title : 'UI Kleurenkiezer',
+		preview : 'Live voorbeeld',
+		config : 'Plak deze tekst in jouw config.js bestand',
+		predefined : 'Voorgedefinieerde kleurensets'
+	}
+});
