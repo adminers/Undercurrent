0d6db8341cadddcd833349954c1132b42bbdd3de
root <root@iZ0jl0q42o4i1mj8jt0c2cZ>
2025-08-23 18:00:01 +0800

    un-simulation/org/qiaweidata/sv.js /  file update.

--- un-simulation/org/qiaweidata/sv.js
+++ un-simulation/org/qiaweidata/sv.js
@@ -1,1 +1,1 @@
-﻿CKEDITOR.plugins.setLang("font","sv",{fontSize:{label:"Storlek",voiceLabel:"Teckenstorlek",panelTitle:"Teckenstorlek"},label:"Typsnitt",panelTitle:"Typsnitt",voiceLabel:"Typsnitt"});
+﻿CKEDITOR.plugins.setLang("iframe","sv",{border:"Visa ramkant",noUrl:"Skriv in URL för iFrame",scrolling:"Aktivera rullningslister",title:"iFrame-egenskaper",toolbar:"iFrame"});
