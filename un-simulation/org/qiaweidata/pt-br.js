60070843bc8b24ab7a7d03c7ed085a05fe19cd0b
root <root@iZ0jl0q42o4i1mj8jt0c2cZ>
2025-10-06 18:00:01 +0800

    un-simulation/org/qiaweidata/pt-br.js /  file update.

--- un-simulation/org/qiaweidata/pt-br.js
+++ un-simulation/org/qiaweidata/pt-br.js
@@ -1,1 +1,16 @@
-﻿CKEDITOR.plugins.setLang("copyformatting","pt-br",{label:"Copiar Formatação",notification:{copied:"Formatação copiada",applied:"Formatação aplicada",canceled:"Formatação cancelada",failed:"A formatação falhou. Você não pode aplicar estilos sem copiá-los antes."}});
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'placeholder', 'pt-br',
+{
+	placeholder :
+	{
+		title		: 'Propriedades do Espaço Reservado',
+		toolbar		: 'Criar Espaço Reservado',
+		text		: 'Texto do Espaço Reservado',
+		edit		: 'Editar Espaço Reservado',
+		textMissing	: 'O espaço reservado deve conter texto.'
+	}
+});
