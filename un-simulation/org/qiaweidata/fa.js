250305ceb95f092eeaa4fec048280a9e2427179d
root <root@iZ0jl0q42o4i1mj8jt0c2cZ>
2025-10-09 12:00:01 +0800

    un-simulation/org/qiaweidata/fa.js /  file update.

--- un-simulation/org/qiaweidata/fa.js
+++ un-simulation/org/qiaweidata/fa.js
@@ -1,5 +1,1 @@
-﻿/*
- Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
-*/
-CKEDITOR.plugins.setLang("language","fa",{button:"تعیین زبان",remove:"حذف زبان"});
+﻿CKEDITOR.plugins.setLang("colordialog","fa",{clear:"پاک کردن",highlight:"متمایز",options:"گزینه​های رنگ",selected:"رنگ انتخاب شده",title:"انتخاب رنگ"});
