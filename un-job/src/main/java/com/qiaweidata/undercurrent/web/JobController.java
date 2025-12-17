62c65a7925adb31747fe5dbf97e8f9b24c84a803
Administrator <1019358770@qq.com>
2025-12-17 14:30:27 +0800

    new job;

---
+++ un-job/src/main/java/com/qiaweidata/undercurrent/web/JobController.java
@@ -0,0 +1,12 @@
+package com.qiaweidata.undercurrent.web;
+
+/**
+ * @Title: JobController
+ * @Description: JobController
+ * @Company:
+ * @author: shenshilong
+ * @date: 2025-12-17
+ * @version: V1.0
+ */
+public class JobController {
+}
