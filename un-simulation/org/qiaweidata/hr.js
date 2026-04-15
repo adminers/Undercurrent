e4af9b1c2fe3c44ab3a57b7c5e65350bc1662736
Administrator <1019358770@qq.com>
2026-04-15 12:00:05 +0800

    un-simulation/org/qiaweidata/hr.js /  file update.

--- un-simulation/org/qiaweidata/hr.js
+++ un-simulation/org/qiaweidata/hr.js
@@ -1,1 +1,16 @@
-﻿CKEDITOR.plugins.setLang("templates","hr",{button:"Predlošci",emptyListMsg:"(Nema definiranih predložaka)",insertOption:"Zamijeni trenutne sadržaje",options:"Opcije predložaka",selectPromptMsg:"Molimo odaberite predložak koji želite otvoriti",title:"Predlošci sadržaja"});
+﻿/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'placeholder', 'hr',
+{
+	placeholder :
+	{
+		title		: 'Svojstva rezerviranog mjesta',
+		toolbar		: 'Napravi rezervirano mjesto',
+		text		: 'Tekst rezerviranog mjesta',
+		edit		: 'Uredi rezervirano mjesto',
+		textMissing	: 'Rezervirano mjesto mora sadržavati tekst.'
+	}
+});
